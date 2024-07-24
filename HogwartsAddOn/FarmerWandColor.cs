adcadb16a6935682c94df5235bb3a7306780fc32
Saah <sarahvivloos@gmail.com>
2024-07-24 05:20:41 -0300

    Change wand sprite index on purchase

---
+++ HogwartsAddOn/FarmerWandColor.cs
@@ -0,0 +1,81 @@
+﻿using StardewValley;
+using StardewValley.Tools;
+
+namespace HogwartsAddOn
+{
+    internal class FarmerWandColor
+    {
+        private const string spriteFlagKey = "HOGWARTSWANDSPRITE";
+        private const string statsFlagKey = "HOGWARTSWANDSTATS";
+        private const string wandOwnerIdKey = "WandOwner";
+        private const string wandOwnerNameKey = "WandOwnerName";
+        private const string wandColorKey = "LastKnownColor";
+        private readonly string ModId;
+        private readonly MeleeWeapon Wand;
+        private Farmer? Owner;
+        internal bool IsWand;
+
+        private static string? GetInfoFromMailData(Farmer? player, string flagKey) => player?.mailReceived.FirstOrDefault(mail => mail.StartsWith(flagKey))?.Replace(flagKey, "");
+
+        internal FarmerWandColor(string modId, MeleeWeapon wand, Farmer? player = null)
+        {
+            ModId = modId;
+            Wand = wand;
+            IsWand = Wand.ItemId.StartsWith($"{ModId}_Wand");
+            Owner = player ?? FindOwner(modId, wand);
+        }
+
+        private static Farmer? FindOwner(string modId, MeleeWeapon wand)
+        {
+            if (!wand.modData.TryGetValue($"{modId}_{wandOwnerIdKey}", out string ownerIdString)) return LastHolderIfWasOwner(wand);
+            if (!long.TryParse(ownerIdString, out long ownerId)) return null;
+            return Game1.getFarmerMaybeOffline(ownerId);
+        }
+
+        private Farmer? LastHolderIfWasOwner(MeleeWeapon wand)
+        {
+            if (wand.getLastFarmerToUse() is Farmer player && PlayerSharesStats(player)) return player;
+            return null;
+        }
+
+        internal bool PlayerSharesStats(Farmer player)
+        {
+            return Wand.ItemId.Split("_")[^1] == GetInfoFromMailData(player, statsFlagKey);
+        }
+
+        private int GetSpriteIndex()
+        {
+            if (Wand.modData.TryGetValue(wandColorKey, out string color) && int.TryParse(color, out int spriteIndex)) return spriteIndex;
+            return int.TryParse(GetInfoFromMailData(Owner, spriteFlagKey), out int mailSpriteIndex) ? mailSpriteIndex : 0;
+        }
+
+        private void UpdateWandOwnerInfo()
+        {
+            UpdateModData(wandOwnerIdKey, Owner!.UniqueMultiplayerID.ToString());
+            UpdateModData(wandOwnerNameKey, Owner.displayName);
+            UpdateModData(wandColorKey, GetSpriteIndex().ToString());
+        }
+
+        private void UpdateModData(string key, string value)
+        {
+            Wand.modData[$"{ModId}_{key}"] = value;
+        }
+
+        private bool IsOwnerInvalid()
+        {
+            if (Owner != null) return false;
+            if (FindOwner(ModId, Wand) is not Farmer owner) return true;
+            Owner = owner;
+            return false;
+        }
+
+        internal void UpdateWand(Farmer? newOwner = null)
+        {
+            if (newOwner != null) Owner = newOwner;
+            else if (IsOwnerInvalid()) return;
+            UpdateWandOwnerInfo();
+            Wand.SetSpriteIndex(GetSpriteIndex());
+            Wand.description = Wand.Description.Replace("{0}", Owner!.displayName);
+        }
+    }
+}
---
+++ HogwartsAddOn/Integrations/UnlockableBundles.cs
@@ -0,0 +1,47 @@
+﻿using StardewValley;
+using StardewValley.Tools;
+
+namespace HogwartsAddOn.Integrations
+{
+    internal class UnlockableBundles
+    {
+        IUnlockableBundlesApi UnlockableBundlesApi { get; set; }
+        string CPModId { get; set; }
+        internal UnlockableBundles(IUnlockableBundlesApi api, string cpModId)
+        {
+            UnlockableBundlesApi = api;
+            CPModId = cpModId;
+            RegisterEventHandlers();
+        }
+
+        internal void RegisterEventHandlers()
+        {
+            UnlockableBundlesApi.BundlePurchasedEvent += OnBundlePurchased;
+        }
+
+        protected void OnBundlePurchased(object sender, IBundlePurchasedEventArgs e)
+        {
+            if (e.Who == null || !e.Bundle.Purchased) return;
+            if (e.Bundle.Key.StartsWith($"{CPModId}_Wand") && GetPlayerWand(e.Who) is FarmerWandColor wand) wand.UpdateWand();
+        }
+
+        protected FarmerWandColor? GetPlayerWand(Farmer player)
+        {
+            if (player.mostRecentlyGrabbedItem is MeleeWeapon maybeWand)
+            {
+                FarmerWandColor wand = new(CPModId, maybeWand, player);
+                if (wand.IsWand && wand.PlayerSharesStats(player)) return wand;
+            }
+            return player.Items
+                .Where(item => item is MeleeWeapon)
+                .Cast<MeleeWeapon>()
+                .Select(weapon => new FarmerWandColor(CPModId, weapon, player))
+                .First(wand => wand.IsWand && wand.PlayerSharesStats(player));
+        }
+
+        internal int GetHousePoints(string house)
+        {
+            return UnlockableBundlesApi.getWalletCurrency($"{CPModId}_{house}", Game1.player.UniqueMultiplayerID);
+        }
+    }
+}
